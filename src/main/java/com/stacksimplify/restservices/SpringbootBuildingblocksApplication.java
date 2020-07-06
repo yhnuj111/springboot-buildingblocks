745a916fa149fa07cdc68088bbf6af44c2d5f123
Zhehang Xu <zxu40@stevens.edu>
2020-07-06 14:18:17 +0800

    2nd

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// fggf
+//
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
